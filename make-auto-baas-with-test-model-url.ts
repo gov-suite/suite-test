7aebd8bcedd0ba2556306afeea75712cb649ad40
Shahid N. Shah <shahid@shah.org>
2020-09-16 20:35:34 -0400

    Resolve Deno 1.4 lint errors

--- make-auto-baas-with-test-model-url.ts
+++ make-auto-baas-with-test-model-url.ts
@@ -3,8 +3,8 @@ import * as autoBaaS from "https://denopkg.com/gov-suite/governed-iac@v0.9.0/mod
 import * as pgsc from "https://denopkg.com/gov-suite/governed-iac@v0.9.0/models/persistence/postgreSQL-engine.service.giac.ts";
 import * as govnImTrSqlDia from "https://denopkg.com/gov-suite/governed-im-transform@v0.6.0/rds/sql/dialect/mod.ts";
 import * as govnImTrSQL from "https://denopkg.com/gov-suite/governed-im-transform@v0.6.0/rds/sql/mod.ts";
-import * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.5.0/core/mod.ts";
-import * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.0.17/mod.ts";
+import type * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.5.0/core/mod.ts";
+import type * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.0.17/mod.ts";
 import * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.5/mod.ts";
 import * as specModule from "https://denopkg.com/shah/specification-module@v1.0.4/mod.ts";
 import * as model from "https://denopkg.com/gov-suite/governed-im-rds@v0.6.0/models/test-model.gim.ts";
