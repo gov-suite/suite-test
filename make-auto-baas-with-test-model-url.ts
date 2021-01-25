d8c8180d683c5ed80eb15a9859a5d477829df8b6
Shahid N. Shah <shahid@shah.org>
2021-01-25 09:47:00 -0500

    fix: update dependencies

--- make-auto-baas-with-test-model-url.ts
+++ make-auto-baas-with-test-model-url.ts
@@ -1,13 +1,13 @@
-import * as giacCore from "https://denopkg.com/gov-suite/governed-iac@v1.1.0/core/mod.ts";
-import * as autoBaaS from "https://denopkg.com/gov-suite/governed-iac@v1.1.0/models/omnibus/middleware-rdbms-api-auto-baas.services.giac.ts";
-import * as pgsc from "https://denopkg.com/gov-suite/governed-iac@v1.1.0/models/persistence/postgreSQL-engine.service.giac.ts";
-import * as govnImTrSqlDia from "https://denopkg.com/gov-suite/governed-im-transform@v0.9.5/rds/sql/dialect/mod.ts";
-import * as govnImTrSQL from "https://denopkg.com/gov-suite/governed-im-transform@v0.9.5/rds/sql/mod.ts";
-import type * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.7.0/core/mod.ts";
-import type * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.1.1/mod.ts";
+import * as giacCore from "https://denopkg.com/gov-suite/governed-iac@v1.1.2/core/mod.ts";
+import * as autoBaaS from "https://denopkg.com/gov-suite/governed-iac@v1.1.2/models/omnibus/middleware-rdbms-api-auto-baas.services.giac.ts";
+import * as pgsc from "https://denopkg.com/gov-suite/governed-iac@v1.1.2/models/persistence/postgreSQL-engine.service.giac.ts";
+import * as govnImTrSqlDia from "https://denopkg.com/gov-suite/governed-im-transform@v0.9.7/rds/sql/dialect/mod.ts";
+import * as govnImTrSQL from "https://denopkg.com/gov-suite/governed-im-transform@v0.9.7/rds/sql/mod.ts";
+import type * as govnImCore from "https://denopkg.com/gov-suite/governed-im@v0.7.1/core/mod.ts";
+import type * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/mod.ts";
 import * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.7/mod.ts";
 import * as specModule from "https://denopkg.com/shah/specification-module@v1.0.9/mod.ts";
-import * as model from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.7/models/test-model.gim.ts";
+import * as model from "https://denopkg.com/gov-suite/governed-im-rds@v0.10.9/models/test-model.gim.ts";
 
 function persistRelatedComposeArtifacts(
   ctx: giacCore.ConfigContext,
